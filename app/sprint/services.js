78b92f381fc83eeeb82bb6fa3d8b09c4183dcd11
AymenBenAmor <aymenbenamor.bardo@gmail.com>
2015-08-27 01:21:26 +0200

    sprint backend fake

--- app/sprint/services.js
+++ app/sprint/services.js
@@ -4,38 +4,17 @@ services.factory('Sprint', [
     function () {
         return {
             query: function () {
-                return [
-                    {
-                        id: 1,
-                        name: 'test',
-                        description: 'test',
-                        startDate: '01/12/2014',
-                        endDate: '01/01/2015'
-                    },
-                    {
-                        id: 2,
-                        name: 'test2',
-                        description: 'test2',
-                        startDate: '01/01/2015',
-                        endDate: '31/01/2015'
-                    },
-                    {
-                        id: 3,
-                        name: 'test4',
-                        description: 'test4',
-                        startDate: '01/01/2015',
-                        endDate: '31/03/2015'
-                    }
-                ];
+                if(window.localStorage.sprints){
+                    return $.map(JSON.parse(window.localStorage.sprints), function(value) {
+                        return value;
+                    });
+                }else{
+                    return [];
+                }
             },
-            getSprint: function () {
-                return {
-                        id: 1,
-                        name: 'test',
-                        description: 'test',
-                        startDate: '01/12/2014',
-                        endDate: '01/01/2015'
-                    };
+            getSprint: function (object) {
+                var sprints = JSON.parse(window.localStorage.sprints);
+                return sprints[object.sprintId];
             }
         };
     }]);
--- app/sprint/sprintController.js
+++ app/sprint/sprintController.js
@@ -6,10 +6,7 @@ angular.module('myApp.sprint', ['ngRoute'])
     $routeProvider.when('/sprint', {
         templateUrl: 'sprint/sprintList.html',
         controller: 'sprintCtrl'
-    }).when('/sprint:sprintId', {
-        templateUrl: 'sprint/sprintEdit.html',
-        controller: 'sprintEditCtrl'
-    }).when('/sprint/new', {
+    }).when('/sprint/:sprintId', {
         templateUrl: 'sprint/sprintEdit.html',
         controller: 'sprintEditCtrl'
     }).otherwise({
@@ -20,6 +17,19 @@ angular.module('myApp.sprint', ['ngRoute'])
 .controller('sprintCtrl', ['$scope', 'Sprint', function($scope, Sprint) {
     $scope.sprints = Sprint.query();
 }])
-.controller('sprintEditCtrl', ['$scope', '$routeParams', 'Sprint', function($scope, $routeParams, Sprint) {
-    $scope.sprint = Sprint.getSprint();
+.controller('sprintEditCtrl', ['$scope', '$routeParams', '$location', 'Sprint',
+        function($scope, $routeParams, $location, Sprint) {
+    if($routeParams.sprintId !== 'new'){
+        $scope.sprint = Sprint.getSprint({sprintId: $routeParams.sprintId});
+    }
+    $scope.save = function(){
+        var sprints = window.localStorage.sprints ? JSON.stringify(window.localStorage.sprints) : {};
+        var nextId = window.localStorage.sprints ? Sprint.query().length : 1;
+        if(!$scope.sprint.id){
+            $scope.sprint.id = nextId;
+        }
+        sprints[$scope.sprint.id] = $scope.sprint;
+        window.localStorage.setItem('sprints', JSON.stringify(sprints));
+        $location.path('/sprint');
+    };
 }]);
